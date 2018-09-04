c9004403490a344500e645e251bd54e6eb3c0e62
Sumon Sarker <csesumonpro@gmail.com>
2018-09-04 23:29:54 +0600

    Data Fetch system added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List Article
+Route::get('articles','ArticleController@index');
+// Single Article
+Route::get('article/{id}','ArticleController@show');
+// Create a Article
+Route::post('article','ArticleController@store');
+// update a Article
+Route::put('articles','ArticleController@store');
+// Delete Articles
+Route::delete('articles','ArticleController@destroy');
+
